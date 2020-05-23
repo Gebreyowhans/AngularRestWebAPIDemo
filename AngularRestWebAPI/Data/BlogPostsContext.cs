5880161a52355daadeae68e9108996810b5b11f2
Gebreyowhans Hailekiros <gebreyohans2005@gmail.com>
2020-05-23 10:17:52 +0300

    commit the backend api

---
+++ AngularRestWebAPI/Data/BlogPostsContext.cs
@@ -0,0 +1,19 @@
+﻿using AngularRestWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularRestWebAPI.Data
+{
+    public class BlogPostsContext : DbContext
+    {
+        public BlogPostsContext(DbContextOptions<BlogPostsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+    }
+}
